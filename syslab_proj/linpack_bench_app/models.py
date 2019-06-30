8799d29f30db33278a5edb739065c4a4d5ee4b9c
Stanley Cheah <sccheah3@gmail.com>
2019-06-29 19:08:17 -0700

    overlaod equals and hash func

--- syslab_proj/linpack_bench_app/models.py
+++ syslab_proj/linpack_bench_app/models.py
@@ -27,5 +27,43 @@ class Linpack(models.Model):
 	answer_result = models.CharField(max_length=20)
 	date_added = models.DateTimeField('date added')
 
+
+	# handle comparison when upload. see same system config and dimms but diff linpack
+
+	def __eq__(self, rhs):
+		if self.tester_name == rhs.tester_name and \
+		   self.N == rhs.N and \
+		   self.NB == rhs.NB and \
+		   self.PMAP == rhs.PMAP and \
+		   self.P == rhs.P and \
+		   self.Q == rhs.Q and \
+		   self.PFACT == rhs.PFACT and \
+		   self.NBMIN == rhs.NBMIN and \
+		   self.NDIV == rhs.NDIV and \
+		   self.RFACT == rhs.RFACT and \
+		   self.BCAST == rhs.BCAST and \
+		   self.DEPTH == rhs.DEPTH and \
+		   self.SWAP == rhs.SWAP and \
+		   self.L1 == rhs.L1 and \
+		   self.U == rhs.U and \
+		   self.EQUIL == rhs.EQUIL and \
+		   self.ALIGN == rhs.ALIGN and \
+		   self.actual_GFLOPS == rhs.actual_GFLOPS and \
+		   self.given_problem == rhs.given_problem and \
+		   self.expected_answer == rhs.expected_answer and \
+		   self.answer_result == rhs.answer_result:
+
+			return True
+
+		else:
+			return False
+
+	def __hash__(self):
+		return hash((self.tester_name, self.N, self.NB, self.PMAP, self.P,
+		   			 self.Q, self.PFACT, self.NBMIN, self.NDIV, self.RFACT,
+		   			 self.BCAST, self.DEPTH, self.SWAP, self.L1, self.U,
+		   			 self.EQUIL, self.ALIGN, self.actual_GFLOPS,
+		   		     self.given_problem, self.expected_answer, self.answer_result))
+
 	def __str__(self):
 		return self.tester_name + ": " + self.answer_result
