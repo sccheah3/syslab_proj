4123994126adb13de27a28ec1fac397f04ec08a6
Stanley Cheah <sccheah3@gmail.com>
2019-08-29 17:04:04 -0700

    implement multiple column filtering

--- syslab_proj/linpack_bench_app/static/linpack_bench_app/js/linpack_db.js
+++ syslab_proj/linpack_bench_app/static/linpack_bench_app/js/linpack_db.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+/*$(document).ready(function () {
 	var systable = $('#sysinfoTable').DataTable({
 		"order": [[12, "desc"]]
 	});
@@ -6,3 +6,31 @@ $(document).ready(function () {
 	console.log(systable.search())
 	console.log("hi")
 });
+*/
+
+$(document).ready(function() {
+    // Setup - add a text input to each footer cell
+    $('tfoot th').each( function () {
+        var title = $(this).text();
+        $(this).html( '<input type="text" style="text-align:center; width:100px" placeholder="' + title +'" />' );
+    } );
+ 
+    // DataTable
+    var table = $('#sysinfoTable').DataTable({
+		"order": [[12, "desc"]]
+	});
+	$('.dataTables_length').addClass('bs-select');
+ 
+    // Apply the search
+    table.columns().every( function () {
+        var that = this;
+ 
+        $( 'input', this.footer() ).on( 'keyup change clear', function () {
+            if ( that.search() !== this.value ) {
+                that
+                    .search( this.value )
+                    .draw();
+            }
+        } );
+    } );
+} );
